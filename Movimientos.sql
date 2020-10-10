40a62a3fab917eea08cf85495898aec4f5c54079
Edwin Leonidas Chacón García <48929379+DigyFenix@users.noreply.github.com>
2020-10-10 02:44:55 -0600

    Add files via upload

---
+++ Movimientos.sql
@@ -0,0 +1,12 @@
+idrh_movimiento,descripcion,porc_sobre_base,monto_fijo,monto,tipo,automatico,activo
+1,"Asociacion solidarista - Ahorro",N,S,0,EGRESO,N,S
+2,"Asociacion solidarista - Tienda",N,S,0,EGRESO,N,S
+3,IGSS,S,N,0.0483,EGRESO,S,S
+4,ISR,S,N,0.05,EGRESO,S,S
+5,"Bonificacion por produccion",N,S,500,INGRESO,N,S
+6,"Sueldo base",N,N,1,INGRESO,S,S
+7,"Hora extra ",N,N,8,INGRESO,N,S
+8,"Hora Extra doble",N,N,16,INGRESO,N,S
+9,"Bonificacion decreto",N,S,250,INGRESO,S,S
+10,"Comision por venta",N,N,0,INGRESO,N,S
+12,"Afilicacion club",S,N,0.001,EGRESO,N,S
